c0576f9f426ec071c7331e05c237b0a0bb3f70d9
Lilit67 <lilitb@comcast.net>
2016-03-09 19:23:16 -0800

    Basic testing added

---
+++ test_inventory_allocator.py
@@ -0,0 +1,36 @@
+import unittest
+import os
+import subprocess
+
+
+
+class InventoryAllocation(unittest.TestCase):
+    
+    
+    def test_1(self):
+        strm = './test1.ixt'
+        inv = './inv_small.txt'
+        golden = '/test1.txt: 1: 1,0,1,0,0,::1,0,1,0,0,::0,0,0,0,0,\n' + \
+           './test1.txt: 2: 0,0,0,0,5,::0,0,0,0,0,::0,0,0,0,5,\n' + \
+                './test1.txt: 3: 0,0,0,4,0,::0,0,0,0,0,::0,0,0,4,0,\n' + \
+                './test1.txt: 4: 1,0,1,0,0,::1,0,0,0,0,::0,0,1,0,0,\n' + \
+                './test1.txt: 5: 0,3,0,0,0,::0,3,0,0,0,::0,0,0,0,0,\n'
+        golden = b'./test1.txt: 1: 1,0,1,0,0,::1,0,1,0,0,::0,0,0,0,0,\n./test1.txt: 2: 0,0,0,0,5,::0,0,0,0,0,::0,0,0,0,5,\n./test1.txt: 3: 0,0,0,4,0,::0,0,0,0,0,::0,0,0,4,0,\n./test1.txt: 4: 1,0,1,0,0,::1,0,0,0,0,::0,0,1,0,0,\n./test1.txt: 5: 0,3,0,0,0,::0,3,0,0,0,::0,0,0,0,0,\n'
+        out = subprocess.check_output(['python', 'inventory_allocator.py'], \
+              stdin=None, stderr=None, shell=False, universal_newlines=False, timeout=None)
+        self.assertEqual(golden, out)
+
+
+    def test_python2To3(self):
+        ret = os.system('python inventory_allocator.py')
+        self.assertEqual(ret, 0)
+        ret = os.system('python3 inventory_allocator.py')
+        self.assertEqual(ret, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
