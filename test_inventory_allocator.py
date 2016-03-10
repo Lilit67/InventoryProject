b07bbd13d6f82c9806c64fbbd961b5cccc27aa37
Lilit67 <lilitb@comcast.net>
2016-03-09 22:35:03 -0800

    Tests fixed after removing option

--- test_inventory_allocator.py
+++ test_inventory_allocator.py
@@ -10,13 +10,15 @@ class InventoryAllocation(unittest.TestCase):
     def test_1(self):
         strm = './test1.ixt'
         inv = './inv_small.txt'
+        '''
         golden = '/test1.txt: 1: 1,0,1,0,0,::1,0,1,0,0,::0,0,0,0,0,\n' + \
            './test1.txt: 2: 0,0,0,0,5,::0,0,0,0,0,::0,0,0,0,5,\n' + \
                 './test1.txt: 3: 0,0,0,4,0,::0,0,0,0,0,::0,0,0,4,0,\n' + \
                 './test1.txt: 4: 1,0,1,0,0,::1,0,0,0,0,::0,0,1,0,0,\n' + \
                 './test1.txt: 5: 0,3,0,0,0,::0,3,0,0,0,::0,0,0,0,0,\n'
+        '''        
         golden = b'./test1.txt: 1: 1,0,1,0,0,::1,0,1,0,0,::0,0,0,0,0,\n./test1.txt: 2: 0,0,0,0,5,::0,0,0,0,0,::0,0,0,0,5,\n./test1.txt: 3: 0,0,0,4,0,::0,0,0,0,0,::0,0,0,4,0,\n./test1.txt: 4: 1,0,1,0,0,::1,0,0,0,0,::0,0,1,0,0,\n./test1.txt: 5: 0,3,0,0,0,::0,3,0,0,0,::0,0,0,0,0,\n'
-        out = subprocess.check_output(['python', 'inventory_allocator.py'], \
+        out = subprocess.check_output(['python', 'inventory_allocator.py', '--orders', './test1.txt'], \
               stdin=None, stderr=None, shell=False, universal_newlines=False)
         self.assertEqual(golden, out)
 
@@ -28,13 +30,13 @@ class InventoryAllocation(unittest.TestCase):
         self.assertEqual(ret, 0)
 
     def test_random_option(self):    
-        ret = os.system('python inventory_allocator.py --random')
+        ret = os.system('python inventory_allocator.py --streams 2')
         self.assertEqual(ret, 0)
-        ret = os.system('python3 inventory_allocator.py --random')
+        ret = os.system('python3 inventory_allocator.py --streams 3')
         self.assertEqual(ret, 0)
 
     def test_random_streams(self):    
-        ret = os.system('python3 inventory_allocator.py --random --streams 4 --inventory inv_large.txt')
+        ret = os.system('python3 inventory_allocator.py --streams 4 --inventory inv_large.txt')
         self.assertEqual(ret, 0)
 
 
